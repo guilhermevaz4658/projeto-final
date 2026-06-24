89126011ec1d1e649a4d135ce6e4728eac01c55b
guilhermevaz4658 <113181158+guilhermevaz4658@users.noreply.github.com>
2026-06-24 16:34:01 -0300

    Update app.js

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,12 +1,11 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const cors = require('cors')
+const cors = require('cors');
 
 app.use(cors());
 app.use(express.json());
 
-const cors = require('cors');
 app.use(cors());
 
 
