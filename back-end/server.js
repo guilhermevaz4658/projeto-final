882b846ccbd2631ed839af73ff7e4cc2df270bce
guilhermevaz4658 <113181158+guilhermevaz4658@users.noreply.github.com>
2026-07-07 16:06:58 -0300

    Update server.js para deploy no render

--- back-end/server.js
+++ back-end/server.js
@@ -1,5 +1,8 @@
 const app = require('./src/app');
 
+// alteração para deploy no render
+const PORT = process.env.PORT || 3000;
+
 app.listen(3000, () => {
     console.log('Servidor rodando em http://localhost:3000');
 });
